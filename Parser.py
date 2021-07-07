f3abd7ff66746b636dcb445709b5bde0cc048196
James Polley <jmspolley@gmail.com>
2021-07-07 15:38:18 -0500

    Create files

---
+++ Parser.py
@@ -0,0 +1,6 @@
+# To implement
+
+# Responsible for:
+#   1. Determining if a given input conforms to a grammar
+#   2. If so, determing structure
+#   3. Else, throw error
---
+++ Tokenizer.py
@@ -0,0 +1,8 @@
+# To implement
+
+# Responsible for:
+#   1. Reading input file(s)
+#   2. Breaks input into tokens
+#   3. Allow easy iteration of tokens (from another module)
+#   4. For current token, returns token value and type
+#   5. Can advance to next token
