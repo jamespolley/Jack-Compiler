1a5a089f835f58ac2e5b18986d1e1d5b38505ad1
James Polley <jmspolley@gmail.com>
2021-07-28 18:52:00 -0500

    Finish implementation

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,3 +1,6 @@
+# TO DO
+#   Move constants/codes to new file
+
 class Tokenizer:
 
     KEYWORD = 0
@@ -18,10 +21,11 @@ class Tokenizer:
         self.token_type = None
 
     def advance(self):
+        """Advances to the next token. Sets token and token_type. Ignores whitespace and comments."""
         self.token = None
         while (self.token == None) and self.has_more_chars():
             # Ignore whitespace and comments
-            if self.file[self.i] == " ":
+            if self.file[self.i].isspace():
                 self.i += 1
             elif self.file[self.i : self.i+2] == "//":
                 self.ignore_line_comment()
@@ -43,14 +47,17 @@ class Tokenizer:
                 self.process_string_constant()
             
             else:
+                print(self.file[self.i])
                 raise Exception(
-                    "Unexpected character: unable to process '{}'"\
+                    "UNEXPECTED CHARACTER: unable to process '{}'"\
                     .format(self.file[self.i]))
 
     def has_more_chars(self):
+        """Returns true if there are more characters to process."""
         return self.i < self.length
     
     def ignore_line_comment(self):
+        """Advances to the end of the current line comment."""
         self.i += 2
         while self.has_more_chars():
             if self.file[self.i] == "\n":
@@ -59,49 +66,57 @@ class Tokenizer:
             self.i += 1
 
     def ignore_open_close_comment(self):
+        """Advances to the end of the current open-close comment."""
         self.i += 2
         while self.has_more_chars():
             if self.file[self.i : self.i+2] == "*/":
                 self.i += 2
                 return
             self.i += 1
+        # TO DO -- raise Exception for comment that didn't close???
     
     def process_keyword_or_identifier(self):
-        # Refactor???
+        """Processes token and identifies as either keyword or identifier. Sets token and token_type."""
         start = self.i
         self.i += 1
         while self.has_more_chars():
-            if not self.file[start : self.i+1].isidentifier(): break
+            if not self.file[start : self.i+1].isidentifier():
+                self.token = self.file[start : self.i]
+                if self.token in self.KEYWORDS:
+                    self.token_type = self.KEYWORD
+                else:
+                    self.token_type = self.IDENTIFIER
+                return
             self.i += 1
-        self.token = self.file[start : self.i]
-        if self.token in self.KEYWORDS:
-            self.token_type = self.KEYWORD
-        else:
-            self.token_type = self.IDENTIFIER
+        raise Exception("UNEXPECTED EOF")
 
     def process_symbol(self):
+        """Processes symbol token. Sets token and token_type."""
         self.token = self.file[self.i]
         self.token_type = self.SYMBOL
         self.i += 1
 
     def process_integer_constant(self):
+        """Processes integer constant token. Sets token and token_type."""
         start = self.i
         self.i += 1
         while self.has_more_chars():
             if not self.file[self.i].isdigit():
-                break
+                self.token = self.file[start : self.i]
+                self.token_type = self.INT_CONSTANT
+                return
             self.i += 1
-        self.token = self.file[start : self.i]
-        self.token_type = self.INT_CONSTANT
+        raise Exception("UNEXPECTED EOF: expected to read ';'")
 
     def process_string_constant(self):
+        """Processes string constant token. Sets token and token_type."""
         start = self.i
         self.i += 1
         while self.has_more_chars():
             if self.file[self.i] == '"':
                 self.i += 1
-                self.token = self.file[start : self.i]
+                self.token = self.file[start+1 : self.i-1]
                 self.token_type = self.STRING_CONSTANT
                 return
             self.i += 1
-        raise Exception("Unexpected end of file: expected to read '\"'")
+        raise Exception("UNEXPECTED EOF: expected to read '\"'")
