0dfe7d165b692dfe85c83abcfdf89f5c5b6b1f65
James Polley <jmspolley@gmail.com>
2021-07-28 18:19:44 -0500

    Finish implementation, improve comments

--- CompilationEngineXML.py
+++ CompilationEngineXML.py
@@ -1,4 +1,5 @@
 # TO DO - include grammar in docstrings
+# TO DO convert < and > to entity references
 
 from Tokenizer import Tokenizer
 
@@ -13,8 +14,8 @@ class CompilationEngineXML:
     TAGS = ["keyword", "identifier", "symbol",
         "integerConstant", "stringConstant"]
     
-    OP = "+-*/=&|<>"
-    UNARY_OP = "-~"
+    OP = ("+", "-", "*", "/", "=", "&", "|", "<", ">")
+    UNARY_OP = ("-", "~")
 
     KEYWORD_CONSTANT = ("true", "false", "null", "this")
     
@@ -24,17 +25,13 @@ class CompilationEngineXML:
         self.tab_level = 0
     
     def compile(self):
+        """Compiles Jack code into XML code (which identifies its lexical elements and grammar)."""
+        self.tokenizer.advance()
         while self.tokenizer.has_more_chars():
-            self.tokenizer.advance()
             self.compile_class()
-        print(self.xml)
     
     def compile_class(self):
-        """
-        Compile a complete class.
-
-        'class' className '{' classVarDec* subroutineDec* '}'
-        """
+        """Compiles a complete class."""
         self.open_tag("class")
         self.expect("class")
         self.expect(self.IDENTIFIER)
@@ -45,11 +42,9 @@ class CompilationEngineXML:
         self.close_tag("class")
         
     def compile_class_var_dec(self):
-        """
-        Compile a static variable declaration, or a field declaration.
-        """
-        # OPTIONAL classVarDec
-        if not self.tokenizer.token in ("static", "field"): return
+        """Compiles a static variable declaration, or a field declaration."""
+        if not self.tokenizer.token in ("static", "field"):
+            return # classVarDec is optional
         self.open_tag("classVarDec")
         self.expect(("static", "field"))
         self.expect(("int", "char", "boolean")) # OR className
@@ -62,13 +57,10 @@ class CompilationEngineXML:
         self.compile_class_var_dec() # for multiple classVarDec
 
     def compile_subroutine_dec(self):
-        """
-        Compile a complete method, function, or constructor.
-        """
-        # OPTIONAL subroutineDec
+        """Compiles a complete method, function, or constructor."""
         if not self.tokenizer.token in \
         ("constructor", "function", "method"):
-            return
+            return # subroutineDec is optional
         self.open_tag("subroutineDec")
         self.expect(("constructor", "function", "method"))
         self.expect(("void", "int", "char", "boolean")) # OR className
@@ -80,40 +72,31 @@ class CompilationEngineXML:
         self.close_tag("subroutineDec")
 
     def compile_parameter_list(self):
-        """
-        Compile a (possibly empty) parameter list. Does not handle the enclosing "()".
-
-        ( (type varName) (',' type varName)* )?
-        """
-        if not self.tokenizer.token_type in \
-        (self.KEYWORD, self.IDENTIFIER):
-            self.open_close_tag(self.TAGS[self.tokenizer.token_type])
-            return # TO DO ---------- method for determining if a type?
+        """Compiles a (possibly empty) parameter list. Does not handle the enclosing "()"."""
         self.open_tag("parameterList")
-        self.expect(("int", "char", "boolean")) # OR className
-        self.expect(self.IDENTIFIER)
-        while self.tokenizer.token == ",":
-            self.expect(",")
+        if self.tokenizer.token != ")":
             self.expect(("int", "char", "boolean")) # OR className
             self.expect(self.IDENTIFIER)
+            while self.tokenizer.token == ",":
+                self.expect(",")
+                self.expect(("int", "char", "boolean")) # OR className
+                self.expect(self.IDENTIFIER)
+            # return # TO DO ------ method for determining if a type?
         self.close_tag("parameterList")
 
     def compile_subroutine_body(self):
-        """
-        Compile a subroutine's body.
-        """
+        """Compiles a subroutine's body."""
         self.open_tag("subroutineBody")
         self.expect("{")
-        self.compile_var_dec() # TO DO -----
-        self.compile_statements() # TO DO -----
+        self.compile_var_dec()
+        self.compile_statements()
         self.expect("}")
         self.close_tag("subroutineBody")
 
     def compile_var_dec(self):
-        """
-        Compile a variable declaration.
-        """
-        if self.tokenizer.token != "var": return
+        """Compiles a variable declaration."""
+        if self.tokenizer.token != "var":
+            return # varDec is optional
         self.open_tag("varDec")
         self.expect("var")
         self.expect(("int", "char", "boolean")) # OR className
@@ -123,28 +106,23 @@ class CompilationEngineXML:
             self.expect(self.IDENTIFIER)
         self.expect(";")
         self.close_tag("varDec")
-        self.compile_var_dec()
+        self.compile_var_dec() # for multiple varDec
 
     def compile_statements(self):
-        """Compile a sequence of statements. Does not handle the enclosing "{}"."""
+        """Compiles a sequence of statements. Does not handle the enclosing "{}"."""
         self.open_tag("statements")
-        token = self.tokenizer.token
-        has_more_statements = True
-        while has_more_statements:
+        while True:
+            token = self.tokenizer.token
             if token == "let": self.compile_let_statement()
             elif token == "if": self.compile_if_statement()
             elif token == "while": self.compile_while_statement()
             elif token == "do": self.compile_do_statement()
             elif token == "return": self.compile_return_statement()
-            else: has_more_statements = False
+            else: break
         self.close_tag("statements")
 
     def compile_let_statement(self):
-        """
-        Compile a let statement.
-
-        'let' varName ( '[' expression ']')? '=' expression ';'
-        """
+        """Compiles a let statement."""
         self.open_tag("letStatement")
         self.expect("let")
         self.expect(self.IDENTIFIER)
@@ -158,9 +136,7 @@ class CompilationEngineXML:
         self.close_tag("letStatement")
 
     def compile_if_statement(self):
-        """
-        Compile an if statement.
-        """
+        """Compiles an if statement."""
         self.open_tag("ifStatement")
         self.expect("if")
         self.expect("(")
@@ -177,9 +153,7 @@ class CompilationEngineXML:
         self.close_tag("ifStatement")
 
     def compile_while_statement(self):
-        """
-        Compile a while statement.
-        """
+        """Compiles a while statement."""
         self.open_tag("whileStatement")
         self.expect("while")
         self.expect("(")
@@ -191,7 +165,7 @@ class CompilationEngineXML:
         self.close_tag("whileStatement")
 
     def compile_do_statement(self):
-        """Compile a do statement."""
+        """Compiles a do statement."""
         self.open_tag("doStatement")
         self.expect("do")
         self.expect(self.IDENTIFIER)
@@ -205,23 +179,32 @@ class CompilationEngineXML:
         self.close_tag("doStatement")
 
     def compile_return_statement(self):
-        """Compile a return statement."""
+        """Compiles a return statement."""
         self.open_tag("returnStatement")
         self.expect("return")
-        self.compile_expression_list()
+        if self.tokenizer.token != ";":
+            self.compile_expression_list()
         self.expect(";")
         self.close_tag("returnStatement")
 
     def compile_expression(self):
-        """Compile an expression."""
-        success = self.compile_term()
+        """Compiles an expression."""
+        if self.tokenizer.token in ");": # OTHER EXPRESSION EXITS???!!!
+            return # expression is optional
+        self.open_tag("expression")
+        self.compile_term()
         while self.tokenizer.token in self.OP:
-            self.expect(self.OP)
+            token = self.tokenizer.token
+            if token == "<": self.expect_special("&lt;")
+            elif token == ">": self.expect_special("&gt;")
+            else: self.expect(self.OP)
             self.compile_term()
-        return success
+        self.close_tag("expression")
 
     def compile_term(self):
-        """Compile a term. If the current token is an identifier, distinguishes between a variable, an array entry, or a subroutine call."""
+        """Compiles a term. If the current token is an identifier, distinguishes between a variable, an array entry, or a subroutine call."""
+        if self.tokenizer.token in ");": return
+        self.open_tag("term")
         token = self.tokenizer.token
         token_type = self.tokenizer.token_type
         if token_type == self.INT_CONSTANT:
@@ -232,9 +215,7 @@ class CompilationEngineXML:
             self.expect(self.KEYWORD)
         elif token_type == self.IDENTIFIER:
             self.expect(self.IDENTIFIER)
-            # In case of subroutine call
-            # BELOW CAN BE SIMPLIFIED WITHOUT LOSING FUNCTIONALITY???
-            if self.tokenizer.token == ".":
+            if self.tokenizer.token == ".": # In case of subroutine call
                 self.expect(".")
                 self.expect(self.IDENTIFIER)
                 self.expect("(")
@@ -244,7 +225,10 @@ class CompilationEngineXML:
                 self.expect("(")
                 self.compile_expression_list()
                 self.expect(")")
-            return
+            elif self.tokenizer.token == "[":
+                self.expect("[")
+                self.compile_expression()
+                self.expect("]")
         elif token == "(":
             self.expect("(")
             self.compile_expression()
@@ -257,54 +241,62 @@ class CompilationEngineXML:
             self.expect(self.UNARY_OP)
             self.compile_term()
         else:
-            return False # NOT A TERM
-            # raise Exception() # TO DO ----------
-        return True
+            raise Exception() # TO DO ----------
+        self.close_tag("term")
 
     def compile_expression_list(self):
-        """Compile a (possibly empty) comma-separated list of expressions."""
-        # TO DO - XML tags
-        success = self.compile_expression() # is this necessary further up the chain???
+        """Compiles a (possibly empty) comma-separated list of expressions."""
+        self.open_tag("expressionList")
+        self.compile_expression()
         while self.tokenizer.token == ",":
             self.expect(",")
             self.compile_expression()
+        self.close_tag("expressionList")
 
     def expect(self, expected):
+        """Checks if a token is valid. Is so, generate XML. Advance."""
         token = self.tokenizer.token
         token_type = self.tokenizer.token_type
 
         if type(expected) is tuple:
             if not token in expected:
                 if token_type != self.IDENTIFIER:
-                    raise Exception() # TO DO ----------
+                    raise Exception(
+                        "INVALID TOKEN: expected {} or identifier"\
+                        .format(expected))
         elif type(expected) is int:
-            if not token_type == expected:
-                raise Exception() # TO DO ----------
-        elif token == None: return # End of File
+            if token_type != expected:
+                raise Exception(
+                    "INVALID TOKEN: expected " + self.TAGS[token_type])
         elif token != expected:
-            raise Exception() # TO DO ----------
-        
+            raise Exception(
+                "INVALID TOKEN: expected '{}'".format(expected))
+        elif token == None:
+            return # End of File
         self.open_close_tag(self.TAGS[token_type], token)
-        print(self.tokenizer.token)
-        print(self.tokenizer.token_type)
         self.tokenizer.advance()
-        print(self.xml)
+    
+    def expect_special(self, token):
+        """Generates XML without checking for validity. Advance."""
+        token_type = self.tokenizer.token_type
+        self.open_close_tag(self.TAGS[token_type], token)
+        self.tokenizer.advance()
 
     def open_tag(self, tag_name):
-        self.xml.append("\t"*self.tab_level)
-        self.xml.append("<{}>\n".format(tag_name))
+        """Generates an open XML tag."""
+        tag = "{0}<{1}>\n".format("\t"*self.tab_level, tag_name)
+        self.xml.append(tag)
         self.tab_level += 1
     
     def close_tag(self, tag_name):
+        """Generates a close XML tag."""
         self.tab_level -= 1
-        self.xml.append("\t"*self.tab_level)
-        self.xml.append("</{}>\n".format(tag_name))
+        self.xml.append(
+            "{0}</{1}>\n".format("\t"*self.tab_level, tag_name))
     
-    def open_close_tag(self, tag_name, value=""):
-        if value != "": value = " " + value
-        self.xml.append("\t"*self.tab_level)
-        self.xml.append("<{0}>{1} </{0}>\n".format(tag_name, value))
-
-# test
-ja = CompilationEngineXML('class Main { function void main(){var Array a; var int length; var int i, sum;}}')
-ja.compile()
+    def open_close_tag(self, tag_name, value=" "):
+        """Generates open and close XML tags on a single line."""
+        if value != " ": value = " {} ".format(value)
+        self.xml.append(
+            "{0}<{1}>{2}</{1}>\n".format(
+                "\t"*self.tab_level, tag_name, value))
